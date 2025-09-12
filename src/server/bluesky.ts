8e7d818c1d00f94a78996d1fa6335948391ffb8e
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-12 21:35:10 +0900

    블루스카이 글 쓰기 기능

--- src/server/bluesky.ts
+++ src/server/bluesky.ts
@@ -1,3 +1,13 @@
+import { Agent, RichText } from "@atproto/api";
+import {
+  isRecord as isPost,
+  validateRecord as validatePost,
+} from "@atproto/api/dist/client/types/app/bsky/feed/post";
+import {
+  isCreate,
+  validateCreate,
+} from "@atproto/api/dist/client/types/com/atproto/repo/applyWrites";
+import { TID } from "@atproto/common";
 import { createServerFn } from "@tanstack/react-start";
 import { z } from "zod";
 
@@ -52,3 +62,68 @@ export const blueskySignOut = createServerFn({
 
   return {};
 });
+
+const writerFormSchema = z.object({
+  content: z.string().min(1, "내용을 입력해주세요"),
+});
+
+export const blueskyPost = createServerFn({
+  method: "POST",
+})
+  .validator((data: unknown) => {
+    return writerFormSchema.parse(data);
+  })
+  .handler(async (ctx) => {
+    const { content } = ctx.data;
+
+    const session = await getOptionalSession();
+
+    if (!session.data.bluesky) {
+      throw new Error("Bluesky 계정이 없습니다.");
+    }
+
+    const client = await blueskyClient.restore(session.data.bluesky.id);
+    const agent = new Agent(client);
+
+    const rkey = TID.nextStr();
+
+    const rt = new RichText({ text: content });
+    await rt.detectFacets(agent);
+
+    const post = {
+      $type: "app.bsky.feed.post",
+      text: rt.text,
+      facets: rt.facets,
+      langs: ["ko"],
+      createdAt: new Date().toISOString(),
+    };
+
+    const writes = {
+      $type: "com.atproto.repo.applyWrites#create",
+      collection: "app.bsky.feed.post",
+      rkey,
+      value: post,
+    };
+
+    const postValidation = validatePost(post);
+    const writeValidation = validateCreate(writes);
+
+    if (
+      postValidation.success &&
+      writeValidation.success &&
+      isPost(post) &&
+      isCreate(writes)
+    ) {
+      await agent.com.atproto.repo.applyWrites({
+        repo: session.data.bluesky.id,
+        validate: true,
+        writes: [writes],
+      });
+    } else {
+      throw new Error("Post validation failed");
+    }
+
+    return {
+      id: rkey,
+    };
+  });
