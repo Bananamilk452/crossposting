aa4b5ae5364090317f37a47c92a9e25356a1f6fa
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-13 22:11:24 +0900

    도커 환경에서 프리즈마 작동 안하는 버그 수정

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma-app/client";
 import { singleton } from "~/utils/singleton";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const prisma = singleton("prisma", () => new PrismaClient());
+const prisma = singleton(
+  "prisma",
+  () => new PrismaClient({ datasourceUrl: process.env.DATABASE_URL }),
+);
 prisma.$connect();
 
 export { prisma };
