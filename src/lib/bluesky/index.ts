441b83152a6d24692a09309bca660431c74d8762
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-15 10:37:03 +0900

    전체적으로 업로드 전 resize 거치게, 특히 블루스카이는 970KB 제한.

--- src/lib/bluesky/index.ts
+++ src/lib/bluesky/index.ts
@@ -1,7 +1,9 @@
 import { Agent } from "@atproto/api";
 import { imageSize } from "image-size";
 
+import { BLUESKY } from "~/constants";
 import { blueskyClient } from "~/lib/bluesky/client";
+import { resizeImage } from "~/server/utils";
 
 export async function getAgent(did: string) {
   const session = await blueskyClient.restore(did);
@@ -11,10 +13,12 @@ export async function getAgent(did: string) {
 }
 
 export async function blueskyUploadBlob(agent: Agent, file: File) {
-  const upload = await agent.uploadBlob(new Blob([file], { type: file.type }));
+  const image = await resizeImage(file, BLUESKY.MAX_IMAGE_SIZE);
+  const sizes = imageSize(image);
 
-  const arrayBuffer = await file.arrayBuffer();
-  const sizes = imageSize(Buffer.from(arrayBuffer));
+  const upload = await agent.uploadBlob(
+    new Blob([image], { type: "image/webp" }),
+  );
 
   return {
     alt: "",
--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -1,8 +1,12 @@
 import ky from "ky";
 
+import { resizeImage } from "~/server/utils";
+
 export async function twitterUploadMedia(accessToken: string, file: File) {
+  const image = new Blob([await resizeImage(file)], { type: "image/webp" });
+
   const formData = new FormData();
-  formData.append("media", file);
+  formData.append("media", image);
   formData.append("media_category", "tweet_image");
 
   const res = await ky
